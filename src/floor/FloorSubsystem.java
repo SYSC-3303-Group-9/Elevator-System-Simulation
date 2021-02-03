87394fd3df7d7df12dcf358dd59ec394e44ae31c
Liya Abebe <43866253+liyaabebe@users.noreply.github.com>
2021-02-03 12:21:43 -0500

    initial commit for floorsubsystem implementation

--- src/floor/FloorSubsystem.java
+++ src/floor/FloorSubsystem.java
@@ -1,11 +1,38 @@
 package floor;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+import main.Main;
+
 public class FloorSubsystem implements Runnable {
+
+	//TODO update syntax after Buffer.java is implemented
+	private Buffer floorandSchedulerBuffer;
+
+	public FloorSubsystem(Buffer floorandSchedulerBuffer)
+	    {
+	        this.floorandSchedulerBuffer = floorandSchedulerBuffer;
+	    }
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		File inputFile = new File(Main.class.getResource("/input.txt").getFile());
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(inputFile));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		List<InputData> data = InputParser.parse(reader);
+		for (InputData x : data) {
+			floorandSchedulerBuffer.put(x);
+			System.out.println(x);
+		}
+
 	}
 
 }
--- test/floor/FloorSubsystemTest.java
+++ test/floor/FloorSubsystemTest.java
@@ -1,5 +1,21 @@
 package floor;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
 public class FloorSubsystemTest {
 
+	@Test
+	void shouldAddDataToBuffer() {
+		// arrange
+		Buffer buffer = new Buffer;
+		FloorSubsystem obj1 = new FloorSubsystem(buffer);
+		
+		// act
+		int result = buffer.size();
+
+		// assert
+		assertEquals(result,3);
+	}
 }
