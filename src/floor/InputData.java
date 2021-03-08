79b12da3a086ed546242eb12195c201239c73e1f
Aubin Musingya <34170066+aubins11@users.noreply.github.com>
2021-03-08 15:38:46 -0500

    Added toAndFromBytes tests; small tweak to fromByte
    revising commit

--- src/floor/InputData.java
+++ src/floor/InputData.java
@@ -14,12 +14,13 @@ public class InputData {
 	private int currentFloor;
 	private Direction direction;
 	private int destinationFloor;
-	
+
 	/**
 	 * Creates a new instance of the InputData class.
-	 * @param time The time of the event.
-	 * @param currentFloor The floor that needs servicing.
-	 * @param direction The direction the elevator must move.
+	 * 
+	 * @param time             The time of the event.
+	 * @param currentFloor     The floor that needs servicing.
+	 * @param direction        The direction the elevator must move.
 	 * @param destinationFloor The floor that the elevator must reach.
 	 */
 	public InputData(LocalTime time, int currentFloor, Direction direction, int destinationFloor) {
@@ -28,100 +29,103 @@ public class InputData {
 		this.direction = direction;
 		this.destinationFloor = destinationFloor;
 	}
-	
+
 	public LocalTime getTime() {
 		return time;
 	}
-	
+
 	public int getCurrentFloor() {
 		return currentFloor;
 	}
-	
+
 	public Direction getDirection() {
 		return direction;
 	}
-	
+
 	public int getDestinationFloor() {
 		return destinationFloor;
 	}
-	
+
 	/**
 	 * Converts an InputData object into a byte array
+	 * 
 	 * @return a byte array that corresponds to the InputData Object
 	 */
-	public byte [] toByte() {
-		
-		//Converting Integer values to byte
-		byte intValues [] = {new Integer(this.time.getHour()).byteValue(), new Integer(this.time.getMinute()).byteValue(),
-				new Integer(this.time.getSecond()).byteValue(), new Integer(this.time.getNano()).byteValue(),
-				new Integer(this.currentFloor).byteValue(), new Integer(this.destinationFloor).byteValue()};
-		
-		//Converting enums to byte
-		byte toDirection [] = this.direction.name().getBytes();
-		
-		byte toBytes [] = new byte[intValues.length + toDirection.length];
-		
-		//Inserting byte values in one byte array
+	public byte[] toBytes() {
+
+		// Converting Integer values to byte
+		byte intValues[] = { new Integer(this.time.getHour()).byteValue(),
+				new Integer(this.time.getMinute()).byteValue(), new Integer(this.time.getSecond()).byteValue(),
+				new Integer(this.time.getNano()).byteValue(), new Integer(this.currentFloor).byteValue(),
+				new Integer(this.destinationFloor).byteValue() };
+
+		// Converting enums to byte
+		byte toDirection[] = this.direction.name().getBytes();
+
+		byte toBytes[] = new byte[intValues.length + toDirection.length];
+
+		// Inserting byte values in one byte array
 		System.arraycopy(intValues, 0, toBytes, 0, intValues.length);
 		System.arraycopy(toDirection, 0, toBytes, 5, toDirection.length);
-		
-		//Re-enter last byte in array (Last byte maybe be removed after System.arraycopy)
+
+		// Re-enter last byte in array (Last byte maybe be removed after
+		// System.arraycopy)
 		toBytes[intValues.length + toDirection.length - 1] = new Integer(this.destinationFloor).byteValue();
-		
+
 		return toBytes;
 	}
-	
+
 	/**
 	 * Converts a byte array into its corresponding InputData
+	 * 
 	 * @param bytes the byte array to be converted
 	 * @return an InputData object
 	 */
-	public static InputData fromByte(byte [] bytes) {
-		
-		//Order of the data coming from the byte array is known
-		String receivedTime = "0" + String.valueOf(bytes[0]) + ":0" + String.valueOf(bytes[1]) + ":0" + String.valueOf(bytes[2]) +
-				"." + String.valueOf(bytes[3]);
-		
+	public static InputData fromBytes(byte[] bytes) {
+
+		// Order of the data coming from the byte array is known
+		String receivedTime = "0" + String.valueOf(bytes[0]) + ":0" + String.valueOf(bytes[1]) + ":0"
+				+ String.valueOf(bytes[2]) + ".00000000" + String.valueOf(bytes[3]);
+
 		String atFloorReceived = String.valueOf(bytes[4]);
-		
+
 		String toDirectionReceived;
-		
-		if(bytes.length == 8) {
-			byte stringByte [] = {bytes[5], bytes[6]};
+
+		if (bytes.length == 8) {
+			byte stringByte[] = { bytes[5], bytes[6] };
 			toDirectionReceived = new String(stringByte, StandardCharsets.UTF_8);
 		} else {
-			byte stringByte [] = {bytes[5], bytes[6], bytes[7], bytes[8]};
+			byte stringByte[] = { bytes[5], bytes[6], bytes[7], bytes[8] };
 			toDirectionReceived = new String(stringByte, StandardCharsets.UTF_8);
 		}
-		
+
 		String toFloorReceived = (bytes.length == 8) ? String.valueOf(bytes[7]) : String.valueOf(bytes[9]);
-		
+
 		InputData toInputData = new InputData(LocalTime.parse(receivedTime), Integer.parseInt(atFloorReceived),
 				Direction.valueOf(toDirectionReceived), Integer.parseInt(toFloorReceived));
-		
+
 		return toInputData;
-	
+
 	}
-	
+
 	@Override
 	public boolean equals(Object o) {
 		if (o == this) {
 			return true;
 		}
-		
+
 		if (o instanceof InputData) {
-			InputData other = (InputData)o;
-			return getTime().equals(other.getTime())
-					&& getCurrentFloor() == other.getCurrentFloor()
+			InputData other = (InputData) o;
+			return getTime().equals(other.getTime()) && getCurrentFloor() == other.getCurrentFloor()
 					&& getDirection().equals(other.getDirection())
 					&& getDestinationFloor() == other.getDestinationFloor();
 		}
 		return false;
 	}
-	
+
 	@Override
 	public String toString() {
 		return time.toString() + " " + currentFloor + " " + direction.toString() + " " + destinationFloor;
 	}
-	
+
 }
--- test/floor/InputDataTest.java
+++ test/floor/InputDataTest.java
@@ -88,4 +88,48 @@ public class InputDataTest {
 		// arrange
 		assertFalse(result);
 	}
+	
+	@Test
+	void toAndFromBytes() {
+		//arrange
+		InputData obj1 = new InputData(LocalTime.of(1, 1, 1, 1), 1, Direction.UP, 1);
+		InputData obj2 = new InputData(LocalTime.of(1, 1, 1, 1), 1, Direction.DOWN, 2);
+		
+		
+		
+		//act
+		byte toByte1 [] = obj1.toBytes();
+		byte toByte2 [] = obj2.toBytes();
+		InputData toInputData1 = InputData.fromBytes(toByte1);
+		InputData toInputData2 = InputData.fromBytes(toByte2);
+		
+		boolean result1 = toInputData1.equals(obj1);
+		boolean result2 = toInputData2.equals(obj2);
+		
+		//arrange
+		assertTrue(result1);
+		assertTrue(result2);
+		
+	}
+	
+	@Test
+	void differentToAndFromBytes() {
+		//arrange
+		InputData obj1 = new InputData(LocalTime.of(1, 1, 1, 1), 1, Direction.UP, 1);
+		InputData obj2 = new InputData(LocalTime.of(1, 1, 1, 1), 1, Direction.DOWN, 2);
+		
+		//act
+		byte toByte1 [] = obj1.toBytes();
+		byte toByte2 [] = obj2.toBytes();
+		InputData toInputData1 = InputData.fromBytes(toByte1);
+		InputData toInputData2 = InputData.fromBytes(toByte2);
+		
+		boolean result1 = toInputData1.equals(obj2);
+		boolean result2 = toInputData2.equals(obj1);
+		
+		//arrange
+		assertFalse(result1);
+		assertFalse(result2);
+		
+	}
 }
