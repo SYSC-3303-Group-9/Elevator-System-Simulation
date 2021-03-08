0d7ab99651a0abf6b4e1411cc5975079c51e713c
Aubin Musingya <34170066+aubins11@users.noreply.github.com>
2021-03-08 10:00:48 -0500

    Added toByte() and fromByte()

--- src/floor/InputData.java
+++ src/floor/InputData.java
@@ -1,6 +1,8 @@
 package floor;
 
+import java.nio.charset.StandardCharsets;
 import java.time.LocalTime;
+import java.util.Arrays;
 
 import elevator.Direction;
 
@@ -43,6 +45,64 @@ public class InputData {
 		return destinationFloor;
 	}
 	
+	/**
+	 * Converts an InputData object into a byte array
+	 * @return a byte array that corresponds to the InputData Object
+	 */
+	public byte [] toByte() {
+		
+		//Converting Integer values to byte
+		byte intValues [] = {new Integer(this.time.getHour()).byteValue(), new Integer(this.time.getMinute()).byteValue(),
+				new Integer(this.time.getSecond()).byteValue(), new Integer(this.time.getNano()).byteValue(),
+				new Integer(this.currentFloor).byteValue(), new Integer(this.destinationFloor).byteValue()};
+		
+		//Converting enums to byte
+		byte toDirection [] = this.direction.name().getBytes();
+		
+		byte toBytes [] = new byte[intValues.length + toDirection.length];
+		
+		//Inserting byte values in one byte array
+		System.arraycopy(intValues, 0, toBytes, 0, intValues.length);
+		System.arraycopy(toDirection, 0, toBytes, 5, toDirection.length);
+		
+		//Re-enter last byte in array (Last byte maybe be removed after System.arraycopy)
+		toBytes[intValues.length + toDirection.length - 1] = new Integer(this.destinationFloor).byteValue();
+		
+		return toBytes;
+	}
+	
+	/**
+	 * Converts a byte array into its corresponding InputData
+	 * @param bytes the byte array to be converted
+	 * @return an InputData object
+	 */
+	public static InputData fromByte(byte [] bytes) {
+		
+		//Order of the data coming from the byte array is known
+		String receivedTime = "0" + String.valueOf(bytes[0]) + ":0" + String.valueOf(bytes[1]) + ":0" + String.valueOf(bytes[2]) +
+				"." + String.valueOf(bytes[3]);
+		
+		String atFloorReceived = String.valueOf(bytes[4]);
+		
+		String toDirectionReceived;
+		
+		if(bytes.length == 8) {
+			byte stringByte [] = {bytes[5], bytes[6]};
+			toDirectionReceived = new String(stringByte, StandardCharsets.UTF_8);
+		} else {
+			byte stringByte [] = {bytes[5], bytes[6], bytes[7], bytes[8]};
+			toDirectionReceived = new String(stringByte, StandardCharsets.UTF_8);
+		}
+		
+		String toFloorReceived = (bytes.length == 8) ? String.valueOf(bytes[7]) : String.valueOf(bytes[9]);
+		
+		InputData toInputData = new InputData(LocalTime.parse(receivedTime), Integer.parseInt(atFloorReceived),
+				Direction.valueOf(toDirectionReceived), Integer.parseInt(toFloorReceived));
+		
+		return toInputData;
+	
+	}
+	
 	@Override
 	public boolean equals(Object o) {
 		if (o == this) {
@@ -63,4 +123,5 @@ public class InputData {
 	public String toString() {
 		return time.toString() + " " + currentFloor + " " + direction.toString() + " " + destinationFloor;
 	}
+	
 }
