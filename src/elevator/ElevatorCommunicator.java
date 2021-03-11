2e84f2acb7ebb76adecef8a0a87ab212df32252c
Liya Abebe <43866253+liyaabebe@users.noreply.github.com>
2021-03-10 23:15:37 -0500

    Update ElevatorCommunicator.java

--- src/elevator/ElevatorCommunicator.java
+++ src/elevator/ElevatorCommunicator.java
@@ -14,17 +14,15 @@ public class ElevatorCommunicator implements Runnable {
 	private Buffer<ElevatorEvent> packetFromElevatorSubsystem;
 	private DatagramPacket sendPacket, receivePacket;
 	private DatagramSocket sendReceiveSocket;
-	private int port;
 
 	public ElevatorCommunicator(Buffer<ElevatorCommand> packetToElevatorSubsystem,
-			Buffer<ElevatorEvent> packetFromElevatorSubsystem, int port) {
+			Buffer<ElevatorEvent> packetFromElevatorSubsystem) {
 		this.packetToElevatorSubsystem = packetToElevatorSubsystem;
 		this.packetFromElevatorSubsystem = packetFromElevatorSubsystem;
-		this.port = port;
 
 		try {
 			// Construct a Datagram socket and bind it to the specified
-			sendReceiveSocket = new DatagramSocket(port);
+			sendReceiveSocket = new DatagramSocket();
 		} catch (SocketException se) { // Can't create the socket.
 			sendReceiveSocket.close();
 			se.printStackTrace();
@@ -34,40 +32,40 @@ public class ElevatorCommunicator implements Runnable {
 
 	@Override
 	public void run() {
-		// Get new instructions from the scheduler.
-		ElevatorCommand command = packetToElevatorSubsystem.get();
 
-		// If buffer is not empty send instruction to elevatorSubsystem
-		if (command != null) {
-			byte commandByte[]= command.toBytes();
-			 sendPacket = new DatagramPacket(commandByte, commandByte.length);
-			try {
-				sendReceiveSocket.send(sendPacket);
-			} catch (IOException e) {
-				e.printStackTrace();
-				System.exit(1);
-			}
-		}
-
-		
-		
-		// Construct a DatagramPacket for receiving packets form the elevatorSubsystem
 		while (true) {
+
+			// Get new instructions from the scheduler.
+			ElevatorCommand command = packetToElevatorSubsystem.get();
+
+			// If buffer is not empty send instruction to elevatorSubsystem
+			if (command != null) {
+				byte commandByte[] = command.toBytes();
+				sendPacket = new DatagramPacket(commandByte, commandByte.length);
+				try {
+					sendReceiveSocket.send(sendPacket);
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+			}
+
+			// Construct a DatagramPacket for receiving packets form the elevatorSubsystem
 			byte data[] = new byte[8];
 			receivePacket = new DatagramPacket(data, data.length);
 
 			try {
 				// Block until a datagram is received via sendReceiveSocket.
 				sendReceiveSocket.receive(receivePacket);
-				
+
 				// Get new event from the elevatorSubsystem.
 				ElevatorEvent event = ElevatorEvent.fromBytes(receivePacket.getData());
-				
+
 				// If buffer is not empty put event in the scheduler buffer
 				if (event != null) {
 					packetFromElevatorSubsystem.put(event);
 				}
-				
+
 			} catch (IOException e) {
 				e.printStackTrace();
 				System.exit(1);
