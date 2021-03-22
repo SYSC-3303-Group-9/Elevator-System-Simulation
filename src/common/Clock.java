b7b982dddf16f5311f2d0f21924e9505712ea38a
Chrisnguyen3 <chrisnguyen3@cmail.carleton.ca>
2021-03-22 16:38:55 -0400

    Comments

--- src/common/Clock.java
+++ src/common/Clock.java
@@ -1,11 +1,16 @@
 package common;
 public class Clock {
 	private static long startTime;
-	
+	/**
+	 * Starts the simulation clock.
+	 */
 	public static void startClock() {
 		startTime = System.currentTimeMillis();
 	}
-	
+	/**
+	 * Returns the time elapsed since the clock started.
+	 * @return	Time elapsed.
+	 */
 	public static long getTime() {
 		return System.currentTimeMillis() - startTime;
 	}
