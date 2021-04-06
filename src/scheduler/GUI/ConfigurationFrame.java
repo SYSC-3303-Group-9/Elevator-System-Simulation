2aa8a176aabc711ffe792cf029fc1783c60e277b
Liya Abebe <43866253+liyaabebe@users.noreply.github.com>
2021-04-06 14:10:07 -0400

    Update ConfigurationFrame.java

--- src/scheduler/GUI/ConfigurationFrame.java
+++ src/scheduler/GUI/ConfigurationFrame.java
@@ -1,9 +1,10 @@
-package scheduler;
+package scheduler.GUI;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 @SuppressWarnings("serial")
 public class ConfigurationFrame extends JFrame implements ActionListener {
@@ -13,6 +14,15 @@ public class ConfigurationFrame extends JFrame implements ActionListener {
 	 */
 	private JTextField elevators, floors, file;
 
+	/**
+	 * JButton for the open and start buttons
+	 */
+	private JButton open, start;
+
+	/**
+	 * JFileChooser for choosing files
+	 */
+	private JFileChooser fc;
 	/**
 	 * Strings to contain file value from entered from text field
 	 */
@@ -47,6 +57,7 @@ public class ConfigurationFrame extends JFrame implements ActionListener {
 		JPanel panel1 = new JPanel();
 		JLabel label = new JLabel("Number of Elevators");
 		elevators = new JTextField("4", 2); // accepts up to 2 character
+		label.setLabelFor(elevators);
 		panel1.add(label);
 		panel1.add(elevators);
 
@@ -54,21 +65,27 @@ public class ConfigurationFrame extends JFrame implements ActionListener {
 		JPanel panel2 = new JPanel();
 		JLabel label2 = new JLabel("Number of Floors");
 		floors = new JTextField("22", 2); // accepts up to 2 character
-
+		label2.setLabelFor(floors);
 		panel2.add(label2);
 		panel2.add(floors);
 
 		// third panel containing file text field
-		JPanel panel3 = new JPanel();
+		JPanel panel3 = new JPanel((new FlowLayout(FlowLayout.LEFT)));
 		JLabel label3 = new JLabel("Input file path");
-		file = new JTextField("input.txt", 30); // accepts up to 30 character
-		file.setAlignmentX(Component.LEFT_ALIGNMENT);
+		file = new JTextField(20);
+		label3.setLabelFor(file);
+
+		// creating a button for open file
+		fc = new JFileChooser();
+		open = new JButton("Open File");
+		open.addActionListener(this);
+
 		panel3.add(label3);
 		panel3.add(file);
+		panel3.add(open);
 
 		// creating button and adding addActionListener
-		JButton start = new JButton("Start");
-		start.setAlignmentX(Component.CENTER_ALIGNMENT);
+		start = new JButton("Start");
 		start.addActionListener(this);
 
 		panel.add(panel1);
@@ -83,26 +100,43 @@ public class ConfigurationFrame extends JFrame implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent evt) {
-		// verify floor and elevator values are integers, if not display a warning
-		try {
-			elevatorNum = Integer.parseInt(elevators.getText());
-			floorNum = Integer.parseInt(floors.getText());
-		} catch (NumberFormatException e) {
-			JOptionPane.showMessageDialog(frame, "Please ensure Elevator and Floor values are integers!",
-					"Invalid format", JOptionPane.ERROR_MESSAGE);
-		}
-		if (elevatorNum == 0 || floorNum == 0) {
-			JOptionPane.showMessageDialog(frame, "Please ensure Elevator and Floor values are not zero!",
-					"Invalid format", JOptionPane.ERROR_MESSAGE);
-		}
 
-		// get file value from text field
-		inputFile = file.getText();
+		// the the open button is clicked
+		if (evt.getSource() == open) {
+			int returnVal = fc.showOpenDialog(frame);
 
-		// if value is entered for all fields, close frame
-		if (elevatorNum != 0 && floorNum != 0 && inputFile != null) {
-			this.done = true;
-			frame.dispose();
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File selectedFile = fc.getSelectedFile();
+				System.out.print("Opening: " + selectedFile.getAbsolutePath() + ".");
+				file.setText(selectedFile.getAbsolutePath());
+			} else {
+				System.out.print("Open command cancelled by user.");
+			}
+			fc.setSelectedFile(null);
+
+		} else {// the start button is clicked
+
+			// verify floor and elevator values are integers, if not display a warning
+			try {
+				elevatorNum = Integer.parseInt(elevators.getText());
+				floorNum = Integer.parseInt(floors.getText());
+			} catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(frame, "Please ensure Elevator and Floor values are integers!",
+						"Invalid format", JOptionPane.ERROR_MESSAGE);
+			}
+			if (elevatorNum == 0 || floorNum == 0) {
+				JOptionPane.showMessageDialog(frame, "Please ensure Elevator and Floor values are not zero!",
+						"Invalid format", JOptionPane.ERROR_MESSAGE);
+			}
+
+			// get file value from text field
+			inputFile = file.getText();
+
+			// if value is entered for all fields, close frame
+			if (elevatorNum != 0 && floorNum != 0 && inputFile != null) {
+				this.done = true;
+				frame.dispose();
+			}
 		}
 	}
 
